f6502526183bcc78c5eb06831b6f8db6e1b341bd
TechKid <mail@techkid.co.uk>
2010-08-14 17:23:57 +0100

    Working? version of GFS.

--- config/environment.rb
+++ config/environment.rb
@@ -18,7 +18,7 @@ require 'mongo'
 uri = URI.parse(ENV['MONGOHQ_URL'])
 conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
 db = conn.db(uri.path.gsub(/^\//, ''))
-GFS = GridFileSystem.new(db)
+GFS = Mongo::GridFileSystem.new(db)
 
 Rails::Initializer.run do |config|
 
